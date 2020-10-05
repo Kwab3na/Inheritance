90792c62271df121e76d0121f1b4896b5c0f5085
Kwabena <student.k.nkansah-boateng@horizoncolumbus.org>
2020-10-05 09:53:18 -0400

    Example

---
+++ src/org/horizoncolumbus/hs/Main.java
@@ -0,0 +1,10 @@
+package org.horizoncolumbus.hs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car car1 = new Car("BMW", "2 Series Grand Coupe");
+        car1.honk();
+        car1.view();
+    }
+}
---
+++ src/org/horizoncolumbus/hs/Vehicle.java
@@ -0,0 +1,14 @@
+package org.horizoncolumbus.hs;
+
+public class Vehicle {
+    String brand;
+
+    Vehicle(String b){
+        brand = b;
+    }
+
+    public void honk(){
+        System.out.println("tuut tuut");
+    }
+}
+
